ff80fa30a5d5fcb2cf5c3ff32f7c7adcce66c766
isamu <isamu@to-kyo.to>
2024-06-18 06:49:49 +0900

    update server

--- server.py
+++ server.py
@@ -2,10 +2,56 @@ from typing import Any
 
 from fastapi import Body, FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+hostName = "http://localhost:8000"
+urlPath = "/agents"
+
+@app.get("/agents/list")
+def agentList():
+    dataSet = [{
+        "name": "echoAgent",
+        "category": "test",
+    }, {
+        "name": "copyAgent",
+        "category": "test",
+    }]
+    
+    def conv(data):
+        return {
+            "agentId": data.get("name"),
+            "name": data.get("name"),
+            "url": hostName + urlPath + "/" + data.get("name"),
+            "category": data.get("category"),
+            "inputs": [],
+            "output": [],
+            "stream": False,
+        }
+    ret = list(map(conv, dataSet))
+    print(ret)
+    
+    agentsList = { "agents": ret }
+    return JSONResponse(content=agentsList)
+
 
 @app.post("/agents/echoAgent")
 def echo(payload: Any = Body(None)):
@@ -14,10 +60,10 @@ def echo(payload: Any = Body(None)):
     return JSONResponse(content=inputs)
 
 @app.post("/agents/copyAgent")
-def echo(payload: Any = Body(None)):
-    inputs = payload.get("inputs")
+def copy(payload: Any = Body(None)):
+    inputs = payload.get("inputs") 
     
-    return inputs[0]
+    return JSONResponse(content=inputs[0])
 
 
 if __name__ == "__main__":
